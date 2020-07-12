af4d368f97ec63e954094cf4978169564b59f1a4
ryu-461 <dra4666@gmail.com>
2020-07-12 22:33:08 +0900

    タイトル変更

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $diceimage = array('image\dice1.png', 'image\dice2.png', 'image\dice3.png', 'ima
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>dicegame</title>
+  <title>DiceGame</title>
   <!-- favicon -->
   <link rel="shortcut icon" href="image/favicon.ico">
   <!--Bootstrap CSS-->
